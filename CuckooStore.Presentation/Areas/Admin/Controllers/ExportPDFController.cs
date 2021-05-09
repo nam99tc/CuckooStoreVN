90755fed26abc0cbae70fafe4dc55e109177114a
luong dinh nam <nam99tc@gmail.com>
2021-05-09 18:34:38 +0700

    Export Pdf information Order detail.

---
+++ CuckooStore.Presentation/Areas/Admin/Controllers/ExportPDFController.cs
@@ -0,0 +1,28 @@
+﻿using CuckooStore.BusinessLogicLayer;
+using Rotativa;
+using System.Web.Mvc;
+
+namespace CuckooStore.Presentation.Areas.Admin.Controllers
+{
+    public class ExportPDFController : Controller
+    {
+        private readonly IOrderDetailServices _orderDetailServices;
+        private readonly IOrderServices _order;
+        public ExportPDFController(IOrderDetailServices orderDetailServices, IOrderServices order)
+        {
+            _order = order;
+            _orderDetailServices = orderDetailServices;
+        }
+        // GET: Admin/ExportPDF
+        public ActionResult PrintPdf(int id)
+        {
+            return new ActionAsPdf("Print",new { id = id });
+        }
+
+        public ActionResult Print(int id)
+        {
+            var or = _order.GetById(id);
+            return View("Print", or);
+        }
+    }
+}
